2e241ec351ff1d75622c592dd6175f26946f202c
leeal <leealexandra58@gmail.com>
2022-07-14 14:09:49 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function getAreaOfCircle(myRadius) {
+    return (myRadius * myRadius * Math.PI);
+}
+console.log(getAreaOfCircle(5));
+
+
+function getCircumferenceOfCircle(myRadius) {
+    return (myRadius * 2 * Math.PI);
+}
+console.log(getCircumferenceOfCircle(10));
+
+
+function getAreaOfSquare(side) {
+    return (side * side);
+}
+console.log(getAreaOfSquare(15));
+
+
+function getAreaOfTriangle(base, height) {
+    return (.5 * base * height);
+}
+console.log(getAreaOfTriangle(20, 25));
